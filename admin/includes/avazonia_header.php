e367d38ccff669d3054cf8d657dfcaa868dc5f9d
Ajigiwe <ajigiwe@gmail.com>
2026-08-04 17:35:03 +0100

    Fix admin dashboard unstyled layout by linking aso.css, admin.css, and admin-avazonia.css in avazonia_header

--- admin/includes/avazonia_header.php
+++ admin/includes/avazonia_header.php
@@ -25,7 +25,8 @@ function av_admin_nav_active($page, $others = []) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="../assets/css/avazonia.css">
+    <link rel="stylesheet" href="../assets/css/aso.css">
+    <link rel="stylesheet" href="../assets/css/admin.css">
     <link rel="stylesheet" href="../assets/css/admin-avazonia.css">
 </head>
 <body>
